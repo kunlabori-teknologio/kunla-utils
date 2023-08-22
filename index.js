259861b6d00e65ca81d0b83fc19cbdfa4a6a81d9
Guest User <Guest@Victors-MacBook-Pro.local>
2023-08-22 20:26:22 -0300

    fix: object const should be string

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const convert = require('./src/controller/utils/convert');
 const array = require('./src/controller/utils/array');
 const file = require('./src/controller/utils/file');
 const object = require('./src/controller/utils/object');
-const object = require('./src/controller/utils/string');
+const string = require('./src/controller/utils/string');
 module.exports = {
   convert,
   array,
