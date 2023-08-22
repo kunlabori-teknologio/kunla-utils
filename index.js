aab1bf32bfe777dd1b14dd15265f77e2921990a9
Guest User <Guest@Victors-MacBook-Pro.local>
2023-08-22 20:19:51 -0300

    fix: call strinf utils in index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const convert = require('./src/controller/utils/convert');
 const array = require('./src/controller/utils/array');
 const file = require('./src/controller/utils/file');
 const object = require('./src/controller/utils/object');
+const object = require('./src/controller/utils/string');
 module.exports = {
   convert,
   array,
